7dd5401b556370aa1ed23d6c2752705a921b73fc
Nelson Frick <nfrick@gmail.com>
2017-09-24 21:45:37 -0300

    WPF - redesigned to use Bindings

--- TreinadorWPF/Exercicio.cs
+++ TreinadorWPF/Exercicio.cs
@@ -1,29 +1,153 @@
-﻿using System.Linq;
-using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
 using System.Xml.Linq;
 
 namespace TreinadorWPF {
-    class Exercicio {
+
+    public enum TipoExercicio {
+        Simples,
+        Alternado,
+        Lado
+    }
+
+    internal class Exercicio : INotifyPropertyChanged {
         public string Nome { get; set; }
-        public RadioButton Tipo { get; set; }
+        public TipoExercicio Tipo { get; set; }
         public int Descanso { get; set; }
         public string Membro { get; set; }
-        public string Lado(string lado) => $@"{Membro} {lado}{Letra}";
         public char Letra => Membro.Last();
+
+        private int _contadorDireito;
+        public int ContadorDireito {
+            get => this._contadorDireito;
+            set {
+                if (this._contadorDireito == value) return;
+                this._contadorDireito = value;
+                this.NotifyPropertyChanged("ContadorDireito");
+            }
+        }
+
+        private int _contadorEsquerdo;
+        public int ContadorEsquerdo {
+            get => this._contadorEsquerdo;
+            set {
+                if (this._contadorEsquerdo == value) return;
+                this._contadorEsquerdo = value;
+                this.NotifyPropertyChanged("ContadorEsquerdo");
+            }
+        }
+        
+        public string Lado { get; set; }
+        public void LadoEsquerdo() {
+            Lado = "esquerd";
+        }
+        public void LadoDireito() {
+            Lado = "direit";
+        }
+        public bool IsEsquerdo => Lado == "esquerd";
+        public bool IsDireito => Lado == "direit";
+
+        public string Seta => IsEsquerdo ? "t" : "u";
+        public Exercicio ProximoExercicio { get; set; }
+
+        public bool IsSimples => Tipo == TipoExercicio.Simples;
+        public bool IsEsquerdoTerminado => ContadorEsquerdo == 3;
+        public bool IsDireitoTerminado => ContadorDireito == 3;
+        public bool IsSerieTerminada =>
+            (IsEsquerdoTerminado && IsDireitoTerminado) ||
+            (IsSimples && IsEsquerdoTerminado);
+
+        private bool IsIniciandoSerie => ContadorEsquerdo == 1 && ContadorDireito == 0;
+        private int ContadorAtual => IsDireito ? ContadorDireito : ContadorEsquerdo;
+
+        public void IncrementaContador(int incremento = 1) {
+            if (IsDireito && ! IsDireitoTerminado)
+                ContadorDireito += incremento;
+            else
+                ContadorEsquerdo += incremento;
+        }
+
+        private string Ordinal => _ordinal[ContadorAtual];
+        private static readonly string[] _ordinal = { "", "primeira", "segunda", "terceira" };
+
+        public string IniciarRepeticao() {
+            switch (Tipo) {
+                case TipoExercicio.Simples:
+                    ContadorEsquerdo += 1;
+                    break;
+                case TipoExercicio.Alternado:
+                    if (IsEsquerdo) {
+                        ContadorEsquerdo += 1;
+                    }
+                    else {
+                        ContadorDireito += 1;
+                    }
+                    break;
+                case TipoExercicio.Lado:
+                    if (IsEsquerdo && !IsEsquerdoTerminado) {
+                        ContadorEsquerdo += 1;
+                    }
+                    else {
+                        ContadorDireito += 1;
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+            return (IsIniciandoSerie ? $"{Nome}. " : string.Empty) + (
+                   IsSimples ? $@"Iniciar {Ordinal} repetição"
+                    : $@"Iniciar {Membro} {Lado}{Letra}, {Ordinal} repetição");
+        }
+
+        public string TerminarRepeticao() {
+            var frase = new StringBuilder(IsSimples
+                ? $@"Terminada {Ordinal} repetição."
+                : $@"Terminad{Letra} {Membro} {Lado}{Letra}, {Ordinal} repetição.");
+
+            if (IsSerieTerminada) {
+                frase.Append(" Série terminada.");
+                frase.Append(ProximoExercicio == null ? @" Fim da sessão." : $" Próximo exercício {ProximoExercicio.Nome}");
+            }
+            else if (Tipo == TipoExercicio.Lado && IsEsquerdo && IsEsquerdoTerminado)
+                frase.Append(@" Lado esquerdo terminado; mudar para lado direito.");
+
+            switch (Tipo) {
+                case TipoExercicio.Simples:
+                    break;
+                case TipoExercicio.Alternado:
+                    Lado = IsEsquerdo ? "direit" : "esquerd";
+                    break;
+                case TipoExercicio.Lado:
+                    if (IsEsquerdo && IsEsquerdoTerminado)
+                        Lado = "direit";
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+            return frase.ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
 
         public Exercicio() { }
 
-        public Exercicio(XElement element, Window1 form) {
+        public Exercicio(XElement element) {
             Nome = element.FirstAttribute.Value;
             var tipo = element.Elements().First().Value;
             Tipo = tipo == "Alternado" ?
-                        form.RadioButtonAlternado :
+                        TipoExercicio.Alternado :
                   (tipo == "Lado" ?
-                        form.RadioButtonLado :
-                        form.RadioButtonSimples);
-
+                      TipoExercicio.Lado :
+                      TipoExercicio.Simples);
             Descanso = int.Parse(element.Elements().ElementAt(1).Value);
             Membro = element.Elements().Last().Value;
+            Lado = "esquerd";
         }
     }
 }
--- TreinadorWPF/Window1.xaml.cs
+++ TreinadorWPF/Window1.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Linq;
 using System.Speech.Synthesis;
-using System.Text;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Threading;
@@ -16,28 +16,16 @@ namespace TreinadorWPF {
     /// </summary>
     public partial class Window1 {
         private readonly DispatcherTimer timer = new DispatcherTimer();
-
-        private string _lado = Esquerdo;
-
         private readonly System.Media.SoundPlayer _playerBing =
             new System.Media.SoundPlayer(Properties.Resources.Bing_bing);
-
         private readonly System.Media.SoundPlayer _playerWhistle =
             new System.Media.SoundPlayer(Properties.Resources.Whistle);
-
         private readonly SpeechSynthesizer _speaker = new SpeechSynthesizer();
-        private const string Direito = @"direit";
-        private const string Esquerdo = @"esquerd";
-        private const string Zero = @"0";
-        private readonly string[] _ordinal = { "", "primeira", "segunda", "terceira" };
 
-        private readonly Exercicio[] _exercicios;
-        private Exercicio _exercicioAtual;
-        private int _contadorSeries;
-        private bool _novaSerie = true;
+        private readonly CollectionView _exercicios;
         private bool _autoMode = true;
 
-        private int K = 1;
+        private const int K = 1;
 
         private Brush ExercicioColor;
         private Brush NumeroAtivoColor;
@@ -53,51 +41,41 @@ namespace TreinadorWPF {
 
             var xml = XDocument.Load($@"{System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)}\Exercicios.xml");
             if (xml.Root != null) {
-                _exercicios = xml.Root.Descendants("Exercicio")
-                    .Select(c => new Exercicio(c, this)).ToArray();
+                _exercicios = new CollectionView(xml.Root.Descendants("Exercicio")
+                    .Select(c => new Exercicio(c)).ToArray());
             }
-            SliderDescanso.Maximum = 60;
-            SliderDescanso.Value = 50;
-            SliderDescanso.Minimum = 20;
-
+            for (var i = 0; i < _exercicios.Count - 2; i++) {
+                ((Exercicio)_exercicios.GetItemAt(i)).ProximoExercicio = (Exercicio)_exercicios.GetItemAt(i + 1);
+            }
+            this.DataContext = _exercicios;
         }
 
         private void Window_ContentRendered(object sender, EventArgs e) {
             ExercicioColor = TextBlockExercicio.Foreground;
             NumeroInativoColor = TextBlockContadorDireito.Foreground;
             NumeroAtivoColor = TextBlockContadorEsquerdo.Foreground;
-            _exercicioAtual = _exercicios[_contadorSeries];
-            MostrarExercício();
             AnunciarInicio();
         }
 
         private void Timer_Tick(object sender, EventArgs e) {
             try {
-                if (ProgressBarDescanso.Value == ProgressBarDescanso.Maximum) throw new Exception();
+                if (ProgressBarDescanso.Value == ProgressBarDescanso.Maximum)
+                    throw new Exception();
                 ProgressBarDescanso.Value++;
-                TextSegundos.Text = $@"{ProgressBarDescanso.Value / K:00} / {ProgressBarDescanso.Maximum / K:00}";
             }
-
             catch (Exception) {
+
                 timer.Stop();
-                TextSegundos.Text = $@"00 / {ProgressBarDescanso.Maximum / K:00}";
-                if (_contadorSeries == _exercicios.Length)
-                    Close();
-
-                if (_novaSerie) {
-                    ResetaContador();
-                    MostrarExercício();
+                if (_autoMode && ((Exercicio)_exercicios.CurrentItem).IsSerieTerminada) {
+                    _exercicios.MoveCurrentToNext();
                 }
-                else if ((SerieAlternado && ContadorEsquerdo != 0) ||
-                        (SerieLado && LadoEsquerdoTerminado))
-                    MudarDeLado();
-
                 ToggleControles();
                 AnunciarInicio();
             }
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e) {
+            var exercicio = (Exercicio)_exercicios.CurrentItem;
             switch (e.Key) {
                 case Key.A:
                     RadioButtonAlternado.IsChecked = true;
@@ -114,7 +92,6 @@ namespace TreinadorWPF {
                     _speaker.Speak(@"Modo automático " + (_autoMode ? "ligado" : "desligado"));
                     break;
                 case Key.I:
-                    ResetaContador();
                     AnunciarInicio();
                     break;
                 case Key.D3:
@@ -130,38 +107,30 @@ namespace TreinadorWPF {
                     SliderDescanso.Value = 60;
                     break;
                 case Key.Add:
-                    _contadorSeries += _contadorSeries == _exercicios.Length - 1 ? 0 : 1;
-                    _exercicioAtual = _exercicios[_contadorSeries];
-                    MostrarExercício();
-                    MostrarLado(false);
+                    _exercicios.MoveCurrentToNext();
+                    if (_exercicios.IsCurrentAfterLast)
+                        _exercicios.MoveCurrentToLast();
                     break;
                 case Key.Subtract:
-                    _contadorSeries -= _contadorSeries == 0 ? 0 : 1;
-                    _exercicioAtual = _exercicios[_contadorSeries];
-                    MostrarExercício();
-                    MostrarLado(false);
+                    _exercicios.MoveCurrentToPrevious();
+                    if (_exercicios.IsCurrentBeforeFirst)
+                        _exercicios.MoveCurrentToFirst();
                     break;
                 case Key.Enter when !_autoMode:
                     AnunciarInicio();
                     break;
                 case Key.Up:
-                    if (ContadorAtualValor < 3) {
-                        ContadorAtual.Text = (ContadorAtualValor + 1).ToString();
-                        MostrarLado(false);
-                    }
+                    exercicio.IncrementaContador();
                     break;
                 case Key.Down:
-                    if (ContadorAtualValor > 0) {
-                        ContadorAtual.Text = (ContadorAtualValor - 1).ToString();
-                        MostrarLado(false);
-                    }
+                    exercicio.IncrementaContador(-1);
                     break;
                 case Key.Left:
-                    _lado = Esquerdo;
+                    exercicio.LadoEsquerdo();
                     MostrarLado(false);
                     break;
                 case Key.Right:
-                    _lado = Direito;
+                    exercicio.LadoDireito();
                     MostrarLado(false);
                     break;
                 default:
@@ -176,47 +145,22 @@ namespace TreinadorWPF {
         }
 
         private void Terminado() {
-            var frase = new StringBuilder(SerieSimples
-                ? $@"Terminada {Ordinal} repetição."
-                : $@"Terminad{_exercicioAtual.Letra} {_exercicioAtual.Lado(_lado)}, {Ordinal} repetição.");
-
-            if ((SerieSimples && LadoEsquerdoTerminado) || LadoDireitoTerminado) {
-                frase.Append(" Série terminada.");
+            var exercicio = (Exercicio)_exercicios.CurrentItem;
+            if (exercicio.IsSerieTerminada) {
                 _playerBing.Play();
                 Thread.Sleep(1000);
-                if (_autoMode) {
-                    ++_contadorSeries;
-                    if (_contadorSeries == _exercicios.Length) {
-                        frase.Append(@" Fim da sessão.");
-                        _speaker.Speak(frase.ToString());
-                        Close();
-                    }
-                    _exercicioAtual = _exercicios[_contadorSeries];
-                }
-                frase.Append($" Próximo exercício {_exercicioAtual.Nome}");
-                _novaSerie = true;
-                ProgressBarDescanso.Maximum = 60 * K;
             }
-            else if (SerieLado && LadoEsquerdoTerminado)
-                frase.Append(@" Lado esquerdo terminado; mudar para lado direito.");
+            _speaker.SpeakAsync(exercicio.TerminarRepeticao());
 
-            _speaker.SpeakAsync(frase.ToString());
-
-            MostrarLado(false);
+            if (exercicio.IsSerieTerminada) {
+                if (_exercicios.CurrentPosition == _exercicios.Count - 1) {
+                    Close();
+                }
+                SliderDescanso.Value = 60;
+            }
             ToggleControles();
             timer.Start();
         }
-
-        private bool SerieSimples => RadioButtonSimples.IsChecked ?? false;
-        private bool SerieAlternado => RadioButtonAlternado.IsChecked ?? false;
-        private bool SerieLado => RadioButtonLado.IsChecked ?? false;
-        private bool LadoEsquerdoTerminado => _lado == Esquerdo && ContadorEsquerdo == 3;
-        private bool LadoDireitoTerminado => _lado == Direito && ContadorDireito == 3;
-        private int ContadorEsquerdo => (int.Parse(TextBlockContadorEsquerdo.Text));
-        private int ContadorDireito => (int.Parse(TextBlockContadorDireito.Text));
-        private int ContadorAtualValor => (int.Parse(ContadorAtual.Text));
-        private TextBlock ContadorAtual => _lado == Direito ? TextBlockContadorDireito : TextBlockContadorEsquerdo;
-        public string Ordinal => _ordinal[ContadorAtualValor];
 
         private void ToggleControles() {
             ProgressBarDescanso.Value = 0;
@@ -227,63 +171,36 @@ namespace TreinadorWPF {
         }
 
         private void AnunciarInicio() {
-            //MostrarExercício();
-            var frase = _novaSerie ? $"{_exercicioAtual.Nome}. " : "";
-            MostrarLado(true);
-            _speaker.Speak(frase
-                + (SerieSimples
-                ? $@"Iniciar {Ordinal} repetição"
-                : $@"Iniciar {_exercicioAtual.Lado(_lado)}, {Ordinal} repetição"));
+            var exercicio = (Exercicio)_exercicios.CurrentItem;
+            _speaker.Speak(exercicio.IniciarRepeticao());
             _playerWhistle.Play();
-            _novaSerie = false;
+            MostrarLado(true);
         }
 
-        private void MostrarExercício() {
-            TextBlockExercicio.Text = _exercicioAtual.Nome;
-            SliderDescanso.Value = _exercicioAtual.Descanso;
-            _exercicioAtual.Tipo.IsChecked = true;
-            SetaDescanso();
-        }
-
-        private void MudarDeLado() {
-            _lado = _lado == Esquerdo ? Direito : Esquerdo;
-        }
 
         private void MostrarLado(bool mostrar) {
-            if (mostrar)
-                ContadorAtual.Text = (ContadorAtualValor + 1).ToString();
-            TextBlockContadorEsquerdo.Foreground = mostrar && _lado == Esquerdo ? NumeroAtivoColor : NumeroInativoColor; //Brushes.Yellow : Brushes.White;
-            TextBlockContadorDireito.Foreground = mostrar && _lado == Direito ? NumeroAtivoColor : NumeroInativoColor; // Brushes.Yellow : Brushes.White;
-            TextBlockSeta.Text = _lado == Esquerdo ? "t" : "u";
+            var exercicio = (Exercicio)_exercicios.CurrentItem;
+            TextBlockContadorEsquerdo.Foreground = mostrar && exercicio.IsEsquerdo ? NumeroAtivoColor : NumeroInativoColor; //Brushes.Yellow : Brushes.White;
+            TextBlockContadorDireito.Foreground = mostrar && exercicio.IsDireito ? NumeroAtivoColor : NumeroInativoColor; // Brushes.Yellow : Brushes.White;
+            TextBlockSeta.Text = exercicio.Seta;
             TextBlockSeta.Visibility = mostrar ? Visibility.Visible : Visibility.Hidden;
-        }
-
-        private void ResetaContador() {
-            TextBlockContadorEsquerdo.Text = Zero;
-            TextBlockContadorDireito.Text = Zero;
-            _lado = Esquerdo;
-        }
-
-        private void SliderDescanso_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            SetaDescanso();
-        }
-
-        private void SetaDescanso() {
-            TextBlockDescanso.Text = $@"Descanso: {SliderDescanso.Value:00}s";
-            ProgressBarDescanso.Maximum = SliderDescanso.Value * K;
-            TextSegundos.Text = $@"00 / {ProgressBarDescanso.Maximum / K:00}";
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e) {
             var rb = sender as RadioButton;
             if (rb == null || !(rb.IsChecked ?? false)) return;
+        }
+    }
 
-            //var counterX = RadioButtonSimples.IsChecked ? 200 : -22;
-            //var setaX = RadioButtonSimples.IsChecked ? 558 : 336;
-            //TextBlockContadorEsquerdo.Margin = = new Thickness(;
-            //TextBlockSeta.Location = new Point(setaX, TextBlockSeta.Location.Y);
+    public class RadioButtonCheckedConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) {
+            return value.Equals(parameter);
+        }
 
-            //TextBlockContadorDireito.Visible = !RadioButtonSimples.IsChecked;
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture) {
+            return value.Equals(true) ? parameter : Binding.DoNothing;
         }
     }
 }
